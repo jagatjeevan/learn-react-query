b72a7bacce8cdc103f27b4b2f9cb1259de61bc25
Jagat Jeevan Sahoo <kanhajeevan@gmail.com>
2024-05-04 13:25:24 +0530

    setup: repo setup of nextjs using pnpm

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Here goes the content of the page</h1>
+    </main>
+  );
+}
